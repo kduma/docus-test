8a5ee6258f2744ef89770410cee6be2c219a549b
Krystian Duma <git@krystian.duma.sh>
2023-03-11 18:57:51 +0100

    Example

---
+++ components/content/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <CodeGroup>
+    <CodeBlock label="Tab 1">
+      <prose-p>
+        Tab 1
+      </prose-p>
+    </CodeBlock>
+    <CodeBlock label="Tab 2">
+      <prose-p>
+        Tab 2
+      </prose-p>
+    </CodeBlock>
+  </CodeGroup>
+</template>
+
+<script>
+export default {
+  name: "Test"
+}
+</script>
+
+<style scoped>
+</style>
